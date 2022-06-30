d0e3ed828eaedde86ffcf3d4444a9eb2329b8306
r.pouya <r.pouya@R-POUYA>
2022-06-30 14:17:43 +0430

    git add covariance

---
+++ CovarianceAndContravariance/Covariance.cs
@@ -0,0 +1,17 @@
+﻿namespace CovarianceAndContravariance
+{
+    public class Covariance
+    {
+        /// <summary>
+        /// more derived type component can be assigned to a less derived type component
+        /// </summary>
+        public void Test()
+        {
+            Fruit fruit = new Fruit();
+            Apple apple = new Apple();
+
+            fruit = apple;
+            apple = fruit; // it has error because we can't add less derived type to more derived type
+        }
+    }
+}
---
+++ CovarianceAndContravariance/Fruit.cs
@@ -0,0 +1,7 @@
+﻿namespace CovarianceAndContravariance
+{
+    public class Fruit
+    {
+
+    }
+}
