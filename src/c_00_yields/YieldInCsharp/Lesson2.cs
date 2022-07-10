e5dcb1a1b1dca2fa1c21a87d1ad4c96ce8e568bf
Reza P <rpouya@hotmail.com>
2022-07-10 16:12:42 +0430

    yield

---
+++ src/c_00_yields/YieldInCsharp/Lesson2.cs
@@ -0,0 +1,49 @@
+﻿using YieldInCsharp.OurCompilerSyntacticSugers;
+
+public static class Lesson2
+{
+    private static IEnumerable<int> Numbers
+    {
+        get
+        {
+            Console.WriteLine("Begin");
+            Console.WriteLine("yielding 1");
+            yield return 1;
+            Console.WriteLine("yielding 4");
+            yield return 4;
+            Console.WriteLine("yielding 2");
+            yield return 2;
+            Console.WriteLine("End !!! ");
+        }
+    }
+
+    public static void Run1()
+    {
+        foreach (int i in Numbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void Run2()
+    {
+        var numbers = new NumbersHybrid();
+
+        foreach (int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    // This show how syntactic suger work for foreach
+    public static void Run()
+    {
+        IEnumerable<int> source = new NumbersHybrid();
+        IEnumerator<int> itrator = source.GetEnumerator();
+
+        while (itrator.MoveNext())
+        {
+            Console.WriteLine(itrator.Current);
+        }
+    }
+}
---
+++ src/c_00_yields/YieldInCsharp/OurCompilerSyntacticSugers/NumbersHybrid.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+namespace YieldInCsharp.OurCompilerSyntacticSugers
+{
+    internal class NumbersHybrid : IEnumerable<int>, IEnumerator<int>
+    {
+        private int _current;
+        private int _state;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public bool MoveNext()
+        {
+            switch (_state)
+            {
+                default: return false;
+                case 0:
+                    {
+                        Console.WriteLine("Begin");
+                        Console.WriteLine("yielding 1");
+                        _current = 1;
+                        _state = 1;
+                        return true;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("yielding 4");
+                        _current = 4;
+                        _state = 2;
+                        return true;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("yielding 2");
+                        _current = 2;
+                        _state = 3;
+                        return true;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("End !!! ");
+                        return false;
+                    }
+            }
+        }
+
+        public int Current => _current;
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        { }
+
+        object IEnumerator.Current => this.Current;
+    }
+}
