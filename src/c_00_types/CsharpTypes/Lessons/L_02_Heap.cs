86daa585dfde1c71df5e2f0906c613b736d9ae72
Reza P <rpouya@hotmail.com>
2022-07-11 23:20:53 +0430

    types

---
+++ src/c_00_types/CsharpTypes/Lessons/L_02_Heap.cs
@@ -0,0 +1,26 @@
+﻿namespace CsharpTypes.Lessons;
+
+internal static class L_02_Heap
+{
+    public class Person
+    {
+        // all members of reference types are placed in heap
+        public string Name;
+        public int Age; 
+
+        public Person(string name, int age )
+        {
+            Name = name;
+        }
+    }
+
+    public static void Run()
+    {
+        int jhoneAge = 20;
+        Person Jhone = new Person("Jhone" , jhoneAge);
+        Person Tom = new Person("Tom" , 23);
+        Person JhoneClone = Jhone;
+    }
+
+    // new in reference type will go to heap, allocate a memory to object and return its address reference
+}
