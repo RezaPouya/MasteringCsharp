8ab00722b2ac2966dde1fc4578d9a8481e7177fa
r.pouya <r.pouya@R-POUYA>
2022-07-14 11:06:16 +0430

    complete

---
+++ IEnumerableTest/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class Program
+{
+    public static void Main()
+    {
+        var numbers = GetNumbers()
+            .Where(p =>
+            {
+                return p % 2 == 0;
+            })
+            .Select(p =>
+            {
+                return p * 1.5;
+            });
+
+        var nums = numbers.ToList();
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        Console.ReadLine();
+    }
+
+    private static IEnumerable<int> GetNumbers()
+    {
+        return Enumerable.Range(0, 100);
+    }
+}
